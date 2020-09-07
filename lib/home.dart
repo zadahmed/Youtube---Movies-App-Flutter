86505c10550c49484cc3a45a960100220347551f
zahidahmed <zadahmed@outlook.com>
2020-09-07 20:46:09 +0100

    Fix: Setup Lists and working with JSON

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key}) : super(key: key);
@@ -8,8 +11,46 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<String> movielist = List<String>();
+  loaddata() async {
+    var url =
+        "https://api.themoviedb.org/3/movie/550?api_key=c874b73c2e165d984925d886da07cbe0";
+    final response = await http.get(url);
+
+    var jsonbody = json.decode(response.body);
+    var page = jsonbody['page'];
+    var results = jsonbody['results'];
+
+    for (int i = 0; i <= results.length; i++) {
+      movielist.add(results[i]['original_title']);
+    }
+
+    setState(() {
+      movielist = movielist;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    loaddata();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Favourite Movies'),
+        backgroundColor: Colors.black,
+      ),
+      body: ListView.builder(
+          itemCount: movielist.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(movielist[index]),
+            );
+          }),
+    );
   }
 }
